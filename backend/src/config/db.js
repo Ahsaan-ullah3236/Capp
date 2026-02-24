6869202a2f07102d408c10a1c86b3597f17f4c60
Ahsan Ullah <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2026-02-24 10:15:47 -0800

    first commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,29 @@
+import pkg from "pg";
+import dotenv from "dotenv"
+const { Pool }  = pkg;
+dotenv.config();
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_HOST);
+
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.on("connect", () => {
+  console.log("✅ PostgreSQL Connected Successfully");
+});
+
+pool.on("error", (err) => {
+  console.error("❌ PostgreSQL Connection Error:", err);
+  process.exit(1);
+});
+
+export default pool;
---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+
+import userRoute from "./routes/userRoutes.js" 
+import errorHandling from "./middleware/errorHandling.js";
+
+
+dotenv .config();
+
+const app = express();
+const port = process.env.PORT || 3001; 
+
+// MiddleWAre 
+app.use(express.json());
+app.use(cors);
+
+// Routes 
+app.use("/api",userRoute)
+
+// Error HAndling middleware 
+app.use(errorHandling)
+
+// TESting 
+app.get("/",async (req, res)=>{
+    const result = await pool.query("SELEct Current_DATABASE()");
+    res.send(`THE DATAbase name is ${result.rows[0].current_database}`)
+})
+
+
+// server Running 
+app.listen(port, ()=>{
+    console.log(`server is running on http://localhost:${port}`);
+})
